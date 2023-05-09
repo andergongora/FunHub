8ca47440970a14af0d67e12aaa21d93fa5c4a21d
Ander Gongora Allue <angona8@alumni.uv.es>
2023-05-09 11:41:17 +0200

    Cracion proyecto

---
+++ store/admin.py
@@ -0,0 +1,106 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Address, Category, Product, Cart, Order, Tag, UserProfile, Favorites
+
+# Register your models here.
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'locality', 'city', 'state')
+    list_filter = ('city', 'state')
+    list_per_page = 10
+    search_fields = ('locality', 'city', 'state')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'category_image', 'is_active', 'is_featured', 'updated_at')
+    list_editable = ('slug', 'is_active', 'is_featured')
+    list_filter = ('is_active', 'is_featured')
+    list_per_page = 10
+    search_fields = ('title', 'description')
+    prepopulated_fields = {"slug": ("title", )}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'product_image', 'is_active', 'is_featured', 'updated_at', 'display_tags')
+    list_editable = ('slug', 'is_active', 'is_featured')
+    list_filter = ('category', 'is_active', 'is_featured')
+    list_per_page = 20
+    search_fields = ('title', 'category__title', 'short_description')
+    filter_horizontal = ('category',)
+    prepopulated_fields = {"slug": ("title", )}
+    
+    def display_tags(self, obj):
+        return ", ".join([tag.tag for tag in obj.tags.all()])
+    display_tags.short_description = "Tags"
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'created_at')
+    list_editable = ('quantity',)
+    list_filter = ('created_at',)
+    list_per_page = 20
+    search_fields = ('user', 'product')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'status', 'ordered_date')
+    list_editable = ('quantity', 'status')
+    list_filter = ('status', 'ordered_date')
+    list_per_page = 20
+    search_fields = ('user', 'product')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'info', 'created_at', 'updated_at')
+    list_editable = ('info',)
+    list_per_page = 10
+    search_fields = ('tag', 'info')
+    list_display_links = ('tag',)
+
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'user profiles'
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+    def tags(self, instance):
+        return ", ".join([tag.tag for tag in instance.userprofile.tags.all()])
+    list_display = UserAdmin.list_display + ('tags',)
+
+
+
+# Define un formulario para agregar tags a un producto en línea.
+class ProductTagInline(admin.TabularInline):
+    model = Product.tags.through
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    # otros atributos del modelo admin
+    inlines = [ProductTagInline,]
+
+
+
+
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product',  'created_at')
+    list_filter = ('created_at',)
+    list_per_page = 20
+    search_fields = ('user', 'product')
+
+
+
+
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Favorites, FavoritesAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
---
+++ store/models.py
@@ -0,0 +1,160 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# Class that contains tags and some information about them
+class Tag(models.Model):
+    tag = models.CharField(max_length=150, verbose_name="User Tag")
+    info = models.TextField(verbose_name="Tag Information")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.tag
+
+
+
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    locality = models.CharField(max_length=150, verbose_name="Nearest Location")
+    city = models.CharField(max_length=150, verbose_name="City")
+    state = models.CharField(max_length=150, verbose_name="State")
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+        ordering = ('locality', )
+    def __str__(self):
+        return self.locality
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Category Title")
+    slug = models.SlugField(max_length=55, verbose_name="Category Slug")
+    description = models.TextField(blank=True, verbose_name="Category Description")
+    category_image = models.ImageField(upload_to='category', blank=True, null=True, verbose_name="Category Image")
+    is_active = models.BooleanField(verbose_name="Is Active?")
+    is_featured = models.BooleanField(verbose_name="Is Featured?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('title', )
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Product Title")
+    slug = models.SlugField(max_length=160, verbose_name="Product Slug")
+    sku = models.CharField(max_length=255, unique=True, verbose_name="Unique Product ID (SKU)")
+    short_description = models.TextField(verbose_name="Short Description")
+    detail_description = models.TextField(blank=True, null=True, verbose_name="Detail Description")
+    product_image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name="Product Image")
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ManyToManyField(Category, verbose_name="Product Categories")
+    tags = models.ManyToManyField(Tag, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active?")
+    is_featured = models.BooleanField(verbose_name="Is Featured?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.title
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Quantity")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    def __str__(self):
+        return str(self.user)
+    
+    # Creating Model Property to calculate Quantity x Price
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
+
+
+STATUS_CHOICES = (
+    ('Pending', 'Pending'),
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('On The Way', 'On The Way'),
+    ('Delivered', 'Delivered'),
+    ('Cancelled', 'Cancelled')
+)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, verbose_name="Shipping Address", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
+    ordered_date = models.DateTimeField(auto_now_add=True, verbose_name="Ordered Date")
+    status = models.CharField(
+        choices=STATUS_CHOICES,
+        max_length=50,
+        default="Pending"
+        )
+
+
+
+# Relationship between User and Tag
+class UserTag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="User")
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name="Tag")
+
+    class Meta:
+        verbose_name_plural = 'User Tags'
+        unique_together = ('user', 'tag')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.tag.tag}"
+    
+
+
+# Agregar el campo user_tags en el modelo User
+User.add_to_class('user_tags', models.ManyToManyField(Tag, through=UserTag, blank=True, verbose_name="User Tags"))
+
+
+
+# Permitir al administrador añadir etiquetas a los usuarios
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+
+
+
+
+
+class Favorites(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = 'Favorites'
+        ordering = ('-created_at', )
+    def __str__(self):
+        return str(self.user)
